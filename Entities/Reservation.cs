1637c2148ac60548490522b96aa7843280bf808b
Vitor Bahir <vitorb82014@gmail.com>
2023-03-24 02:44:35 -0300

    Very bad solution

---
+++ Entities/Reservation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception1_csharp.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            Checkin = checkIn;
+            Checkout = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", Check-in: "
+                + Checkin.ToString("dd/MM/yyyy")
+                + ", Check-out"
+                + Checkout.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
